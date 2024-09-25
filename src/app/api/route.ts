c382581195daee195e90461f4ac7d544ce4622ae
Justin Miguel Reyes <virozzegt@gmail.com>
2024-09-25 17:45:44 +0800

    Add files

---
+++ src/app/api/route.ts
@@ -0,0 +1,32 @@
+import { createOpenAI } from "@ai-sdk/openai";
+import { streamText } from "ai";
+
+export async function POST(req: Request) {
+  const reqBody = await req.json();
+  const prompt = reqBody.data.prompt;
+
+  const openai = createOpenAI({
+    baseURL: "https://api.sambanova.ai/v1/",
+    apiKey: process.env.SAMBANOVA_API_KEY,
+  });
+
+  const systemInstructions = `
+    You are an expert frontend React engineer who is also a great UI/UX designer. Follow all the instructions carefully.
+    - Always respond in a React component, don't  respond as text or message. Respond only in code.
+    - Create a React component for whatever the user asked you to create and make sure it can run by itself by using a default export
+    - Make sure the React app is interactive and functional by creating state when needed and having no required props
+    - If you use any imports from React like useState or useEffect, make sure to import them directly
+    - Use ReactJS as the language for the React component
+    - Use inline CSS for styling. DO NOT USE ARBITRARY VALUES (e.g. \`h-[600px]\`). Make sure to use a consistent color palette.
+    - Use CSS margin and padding classes to style the components and ensure the components are spaced out nicely. Make sure it is responsive, centered, and it looks professional.
+    - Please ONLY return the full React code starting with the imports, nothing else. It's very important for my job that you only return the React code with imports. DO NOT START WITH \`\`\`typescript or \`\`\`javascript or \`\`\`tsx or \`\`\`.
+    - Please dont use any external libraries or packages.
+  `;
+
+  const result = await streamText({
+    model: openai("Meta-Llama-3.1-405B-Instruct"),
+    prompt: `${systemInstructions}\n\n${prompt}`,
+  });
+
+  return result.toDataStreamResponse();
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,157 @@
-import Image from "next/image";
+"use client";
+import { useChat } from "ai/react";
+import { Bot, Loader2, Send, User2 } from "lucide-react";
+import { Sandpack } from "@codesandbox/sandpack-react";
+import { useRef, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
+    useChat({
+      api: "api",
+    });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const chatSectionRef = useRef(null);
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleSubmit(event, {
+      data: {
+        prompt: input,
+      },
+    });
+  };
+
+  useEffect(() => {
+    if (messages.length > 0 && chatSectionRef.current) {
+      (chatSectionRef.current as HTMLElement)?.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
+  return (
+  <main className="min-h-screen bg-gray-900 text-gray-100">
+
+    {/*Hero Section */}
+    <section className="bg-gradient-to-r from-gray-800 to-gray-900 text-white py-24">
+            <div className="container mx-auto px-6 text-center">
+                <div className="mb-4 inline-flex items-center justify-center border border-white rounded-lg px-3 py-1">
+                  <span className="text-gray-300 text-md">
+                    Powered by <span className="font-medium text-white">Llama 3.1</span> and{" "}
+                    <span className="font-medium text-white">NextJS</span>
+                  </span>
+                </div>
+              <h1 className="mb-4 text-5xl font-extrabold leading-tight tracking-tight">
+                Create {" "} <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-600">
+                React </span> components with 
+                <span className="ml-3 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-600">
+                  Llama AI
+                </span>
+              </h1>
+              <p className="mb-12 text-2xl text-gray-300 max-w-xl mx-auto">
+                Harness the power of AI with Meta's flagship model, Llama 3.1 405b with NextJS.
+              </p>
+              
+              <div className="flex justify-center space-x-4">
+                <a
+                  href="#chat"
+                  className="bg-blue-600 text-white px-8 py-3 rounded-lg font-bold hover:bg-blue-900 transition duration-300"
+                >
+                  Start Coding
+                </a>
+                <a
+                  href="https://github.com/jei3m"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-gray-700 text-white px-8 py-3 rounded-lg font-bold hover:bg-gray-800 transition duration-300"
+                >
+                  <FontAwesomeIcon icon={faGithub} className="mr-2" />
+                  View on GitHub
+                </a>
+              </div>
+            </div>
+       </section>
+
+       {/* Chat Section */}
+      <section id="chat" ref={chatSectionRef} className="w-full min-h-screen flex flex-col">
+        <div className="flex-grow bg-gradient-to-r from-gray-800 to-gray-900 p-8">
+          <div className="mx-auto w-full max-w-[800px]">
+          <form
+            onSubmit={handleFormSubmit}
+            className="flex items-center space-x-4 mb-6"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="relative flex-grow">
+              <input
+                type="text"
+                placeholder={isLoading ? "Generating . . ." : "Make me a calendar..."}
+                value={input}
+                disabled={isLoading}
+                onChange={handleInputChange}
+                className="bg-gray-700 border border-gray-600 rounded-[10px] px-6 py-3 text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition w-full"
+              />
+              <button
+                type="submit"
+                className="absolute right-0 top-1/2 -translate-y-1/2 bg-white text-black rounded-[8px] p-3 hover:bg-blue-700 hover:text-white transition duration-300"
+              >
+                {isLoading ? (
+                  <Loader2
+                    onClick={stop}
+                    className="h-6 w-6 animate-spin"
+                  />
+                ) : (
+                  <Send className="h-6 w-6" />
+                )}
+              </button>
+            </div>
+          </form>
+          </div>
+
+          {messages.length > 0 && (
+            <div className="space-y-8">
+              {messages.slice(-2).map((m, index) => (
+                <div
+                  key={index}
+                  className={`flex ${m.role === "assistant" ? "justify-start" : "justify-end"}`}
+                >
+                  <div
+                    className={`relative max-w-full w-full p-5 rounded-lg shadow ${
+                      m.role === "assistant" ? "bg-gray-700" : "bg-gray-600"
+                    }`}
+                  >
+                    {m.role === "assistant" ? (
+                      <Bot className="absolute -top-4 left-4 bg-blue-600 text-white rounded-full p-1" size={24} />
+                    ) : (
+                      <User2 className="absolute -top-4 right-4 bg-gray-500 text-white rounded-full p-1" size={24} />
+                    )}
+                    {m.role === "assistant" ? (
+                      <div className="w-full overflow-hidden">
+                        <Sandpack
+                          template="react"
+                          files={{
+                            "/App.js": m.content,
+                          }}
+                          theme="dark"
+                          options={{
+                            editorHeight: 740,
+                            showLineNumbers: true,
+                          }}
+                          customSetup={{
+                            dependencies: {},
+                          }}
+                        />
+                      </div>
+                    ) : (
+                        <div className="w-full overflow-hidden">
+                          {m.content}
+                        </div>
+                    )}
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+    </main>
   );
 }
